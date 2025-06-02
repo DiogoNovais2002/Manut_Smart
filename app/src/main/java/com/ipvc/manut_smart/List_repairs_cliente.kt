35f71d7e88bd22044ed4f41d80c54f5fecf4ed41
Rafaonline <diogorafaelsantosnovais@gmail.com>
2025-06-02 11:15:43 +0100

    Adicionar janelas

---
+++ app/src/main/java/com/ipvc/manut_smart/List_repairs_cliente.kt
@@ -0,0 +1,32 @@
+package com.ipvc.manut_smart
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class List_repairs_cliente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_repairs_cliente)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnExpand = findViewById<FrameLayout>(R.id.btnExpand)
+        val detailsLayout = findViewById<LinearLayout>(R.id.detailsLayout)
+
+        btnExpand.setOnClickListener {
+            detailsLayout.visibility =
+                if (detailsLayout.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+    }
+
+}
---
+++ app/src/main/java/com/ipvc/manut_smart/Pending_repair.kt
@@ -0,0 +1,34 @@
+package com.ipvc.manut_smart
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Pending_repair : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pending_repair)
+
+        val btnFiltrar = findViewById<Button>(R.id.btnFiltrar)
+        val filterOptions = findViewById<LinearLayout>(R.id.filterOptions)
+        val btnExpand = findViewById<View>(R.id.btnExpand)
+        val detailsLayout = findViewById<LinearLayout>(R.id.detailsLayout)
+
+        btnFiltrar.setOnClickListener {
+            filterOptions.visibility =
+                if (filterOptions.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        btnExpand.setOnClickListener {
+            detailsLayout.visibility =
+                if (detailsLayout.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        findViewById<Button>(R.id.btnReparar).setOnClickListener {
+            Toast.makeText(this, "Reparação iniciada", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
